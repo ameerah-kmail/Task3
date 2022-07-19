4961673ceb0747df2a7b88a155e2fa2030cfd14f
ameerah kmail <ameerahhasan90@gmail.com>
2022-07-19 09:53:58 +0300

    build project

---
+++ test/Repo/empRepo.cs
@@ -0,0 +1,36 @@
+﻿using test.models;
+namespace test.Repo
+{
+    public class empRepo
+    {
+      
+        static List<emp> e{ get; set;}
+
+        static empRepo()
+        {
+            e = new List<emp>()
+            {
+                new emp(){ Id = 1, Name ="a" ,Title="cse",Salary=60000},
+                new emp(){ Id = 2, Name ="b" ,Title="cs",Salary=60000}
+
+        };
+        }
+
+        public static List<emp> getAll ()
+        {
+            return e;
+        }
+        /*
+        public static List<emp> get(int id)
+        {
+            return e.FirstOrDefault(emp=>emp.Id==id);
+        }
+        public static delete(int id)
+        {
+            var e = get(id);
+            if (e == null)
+                return NotFound;
+
+        }*/
+    }
+}
---
+++ test/models/emp.cs
@@ -0,0 +1,11 @@
+﻿
+namespace test.models
+{
+    public class emp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public decimal? Salary { get; set; }//? allow null
+    }
+}
